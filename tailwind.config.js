ca6df3af1b2ca93c60413179ab4ce842d46d232d
Atharv kulkarni <114987636+kulkarniatharv80@users.noreply.github.com>
2025-07-24 23:01:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+}
